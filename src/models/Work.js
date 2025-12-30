5a638c69d96a4228af2f9620859f78d66e073924
WpDavi <100159858+WpDavi@users.noreply.github.com>
2025-12-30 16:07:45 -0300

    Update Work.js

--- src/models/Work.js
+++ src/models/Work.js
@@ -4,14 +4,21 @@ const workSchema = new mongoose.Schema(
   {
     status: { type: String, required: true, trim: true },
     name: { type: String, required: true, trim: true },
+    fone: { type: String, required: true, trim: true },
     startDate: { type: Date },
     endDate: { type: Date },
     amount: { type: Number, default: 0 },
+    serviceType: { type: String, trim: true },
     materialType: { type: String, trim: true },
     finish: { type: String, trim: true },
     description: { type: String },
+    measure: { type: String },
+    entryServiceValue: { type: String },
+    serviceValue: { type: String },
+    delyveryForecast: { type: String },
     image: { type: String },
   },
+
   {
     timestamps: true,
   }
